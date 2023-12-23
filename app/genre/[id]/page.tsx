e0f1ad8b2a3476440774077ea47e74e83de2ef84
frankesomcare <frankline.wakasiaka@esomcare.com>
2023-12-23 13:15:01 +0300

    shadcnn form

---
+++ app/genre/[id]/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+type Props = {
+    params:{
+        id:string,
+    },
+    searchParams:{
+        genre:string
+    }
+}
+
+function GenrePage( {params: {id}, searchParams: {genre}}:Props) {
+  return (
+    <div>GenrePage</div>
+  )
+}
+
+export default GenrePage;
